733a22ef998a0286d09a9cf97d957fcb78f233ed
Ethan Lee <wxliyc@gmail.com>
2022-03-01 00:06:52 -0600

    commit tree 7669e61665404fcefba734573a0671864880fb4c

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function addingEventListener() {
-}
+function addingEventListener(click, clickAlert) {
+
+    const input = document.getElementById('input');
+    
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener(click, clickAlert);
+    }
+    
+    const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
